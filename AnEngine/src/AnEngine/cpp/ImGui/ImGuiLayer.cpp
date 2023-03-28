343f6d2597a0e2428d030fb85ba06a056cd7eebe
UFifty50sh <UFifty50.github@gmail.com>
2023-03-29 00:09:43 +0100

    fixed linux include order and make for windows

--- AnEngine/src/AnEngine/cpp/ImGui/ImGuiLayer.cpp
+++ AnEngine/src/AnEngine/cpp/ImGui/ImGuiLayer.cpp
@@ -6,8 +6,8 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 namespace AnEngine {
     ImGuiLayer::ImGuiLayer() : Layer("ImGuiLayer") {}
